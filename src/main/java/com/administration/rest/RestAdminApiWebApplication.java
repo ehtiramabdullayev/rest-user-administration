c8b7ce61d6e640eba383aa8a94c128dbbe60fa26
eabdullayev <ehtiramabdullayev@gmail.com>
2019-10-20 23:25:51 +0200

    added spring-boot dependencies to the pom file and created the main class

---
+++ src/main/java/com/administration/rest/RestAdminApiWebApplication.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ehtiram Etibarovich Abdullayev on 20.10.2019
+ * @project rest-admin-api
+ */
+@SpringBootApplication
+public class RestAdminApiWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestAdminApiWebApplication.class, args);
+
+    }
+}
+
+
+
+
