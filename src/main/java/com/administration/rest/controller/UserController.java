9b359537d820159d714112668421b9a3e2882873
eabdullayev <ehtiramabdullayev@gmail.com>
2019-10-24 00:07:54 +0200

    refactored versioning and swagger

--- src/main/java/com/administration/rest/controller/UserController.java
+++ src/main/java/com/administration/rest/controller/UserController.java
@@ -2,6 +2,7 @@ package com.administration.rest.controller;
 
 import com.administration.rest.domain.User;
 import com.administration.rest.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,7 @@ import java.util.Set;
  * @author Ehtiram Etibarovich Abdullayev on 21.10.2019
  * @project rest-admin-api
  */
-@RequestMapping("/userServices")
+@RequestMapping("/userServices/v1")
 @RestController
 public class UserController {
 
@@ -22,28 +23,30 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(value = "v1/addUser", method = RequestMethod.POST, consumes = {"application/json"})
+    @ApiOperation(value = "Add a user")
+    @RequestMapping(value = "addUser", method = RequestMethod.POST, consumes = {"application/json"})
     public User addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
-    @GetMapping(value = "v1/listUsers")
+    @ApiOperation(value = "Get the list of users")
+    @GetMapping(value = "listUsers")
     public Set<User> getUsers() {
         return userService.listUsers();
     }
 
 
-    @RequestMapping(value = "v1/changeUser", method = RequestMethod.PUT, consumes = {"application/json"})
+    @RequestMapping(value = "changeUser", method = RequestMethod.PUT, consumes = {"application/json"})
     public User changeUser(@RequestBody User user) {
         return userService.changeUser(user);
     }
 
-    @RequestMapping(value = "v1/getUserById", method = RequestMethod.GET, produces = {"application/json"})
+    @RequestMapping(value = "getUserById", method = RequestMethod.GET, produces = {"application/json"})
     public User findUserById(@RequestBody Long id) {
         return userService.findUserById(id);
     }
 
-    @RequestMapping(value = "v1/deleteUserById", method = RequestMethod.PUT, produces = {"application/json"})
+    @RequestMapping(value = "deleteUserById", method = RequestMethod.PUT, produces = {"application/json"})
     public User deleteUserById(@RequestBody Long id) {
         return userService.deleteUserById(id);
     }
